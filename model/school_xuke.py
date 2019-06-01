8ec24781391aafbc1b52e07a20b8d120639e9f91
fzx <648215109@qq.com>
2019-06-01 09:33:55 +0800

    school_xueke提交

--- model/school_xuke.py
+++ model/school_xuke.py
@@ -1,14 +1,6 @@
 
-import pymysql
+from setting import db
 
-#连接数据库
-try:
-    db = pymysql.connect(host="111.116.20.92",user="ccut",
-        passwd="ccut",
-        db="ccut1",
-        charset='utf8')
-except:
-    print("could not connect to mysql server")
 
 school = []
 def search_school():
